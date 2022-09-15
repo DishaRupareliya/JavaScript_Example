b4d6fe2a0244e350580621508758935df56bfd02
KHYATIBORAD <khyatiborad6402@gmail.com>
2022-09-15 10:41:09 +0530

    add: javascript example

--- validation.php
+++ validation.php
@@ -19,30 +19,35 @@
 							<div class="col-6 col-md-12">
 								<label>Firstname</label>
 								<input type="text" name="firstname" class="form-control" id="firstname">
+								<span id="error1"></span>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-6 col-md-12">
 								<label>Lastname</label>
 								<input type="text" name="lastname" class="form-control" id="lastname">
+								<span id="error2"></span>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-6 col-md-12">
 								<label>Email</label>
-								<input type="email" name="email" class="form-control" id="email" required>
+								<input type="email" name="email" class="form-control" id="email" >
+								<span id="error3"></span>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-6 col-md-12">
 								<label>password</label>
 								<input type="password" name="password" class="form-control" id="password">
+								<span id="error4"></span>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-6 col-md-12">
 								<label>City</label>
 								<input type="text" name="city" class="form-control" id="city">
+								<span id="error5"></span>
 							</div>
 						</div><br>
 						<div class="row">
@@ -57,29 +62,49 @@
 	</div>
 	<script type="text/javascript">
 		function Validation(){
-			var firstname=document.myform.firstname.value;
+			var firstname=document.getElementById('firstname').value;
+			var error=document.getElementById('error1');
 			if(firstname==null || firstname==""){
-				alert("Enter Firstname");
-				//return false;
+				error.textContent="please enter your firstname";
+				return false;
+			}else{
+				error.textContent="";
+				return false;
 			}
-			var lastname=document.myform.lastname.value;
+			var lastname=document.getElementById('lastname').value;
+			var error1=document.getElementById('error2');
 			if(lastname==null || lastname==""){
-				alert("Enter Lastname");
+				error1.textContent="please enter your lastname";
+				return false;
+			}else{
+				error1.textContent="";
 				return false;
 			}
-			var email=document.myform.email.value;
+			var email=document.getElementById('email').value;
+			var error2=document.getElementById('error3');
 			if(email==null || email==""){
-				alert("Enter email");
+				error2.textContent="please enter your email";
+				return false;
+			}else{
+				error2.textContent="";
 				return false;
 			}
-			var password=document.myform.password.value;
+			var password=document.getElementById('password').value;
+			var error3=document.getElementById('error4');
 			if(password==null || password.length<6){
-				alert("Emter 6 digit");
+				error3.textContent="password enter must be 6 char";
+				return false;
+			}else{
+				error3.textContent="";
 				return false;
 			}
-			var city=document.myform.city.value;
+			var city=document.getElementById('city').value;
+			var error4=document.getElementById('error5');
 			if(city==null || city==""){
-				alert("Enter City");
+				error4.textContent="please enter your city";
+				return false;
+			}else{
+				error4.textContent="";
 				return false;
 			}
 		}
